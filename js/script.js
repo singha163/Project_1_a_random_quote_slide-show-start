8eb98c4907b4977f6847441c9165bf1811aa677d
Amarpreet Singh <amarpreet2209@gmail.com>
2022-05-11 22:58:43 -0700

    quotes array created and data added

---
+++ js/script.js
@@ -0,0 +1,61 @@
+/******************************************
+*****************************************/
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [];
+quotes[0]={};
+quotes[1]={};
+quotes[2]={};
+quotes[3]={};
+quotes[4]={};
+
+
+
+quotes[0].quote="A wise man can learn more from his enemies than a fool from his friends.";
+quotes[0].source="Niki Lauda";
+quotes[0].citation="Rush";
+quotes[0].year="2013";
+
+quotes[1].quote="Every man dies, but not every man really lives.";
+quotes[1].source="William Wallace";
+quotes[1].citation="Braveheart";
+quotes[1].year="1995";
+
+quotes[2].quote="Great men are not born great, they grow great.";
+quotes[2].source="Mario Puzo";
+quotes[2].citation="The Godfather";
+quotes[2].year="1972";
+
+quotes[3].quote="Life is not the amount of breaths you take. It's the moments that take your breath away.";
+quotes[3].source="Alex Hitchens";
+quotes[3].citation="Hitch";
+quotes[3].year="2005";
+
+quotes[4].quote="Life moves pretty fast. If you don't stop and look around once in a while, you could miss it.";
+quotes[4].source="Ferris Bueller";
+quotes[4].citation="Ferris Bueller's Day Off";
+quotes[4].year="1986";
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * The code will look like the following. You need to complete it.
+
+***/
+
+//document.getElementById('load-quote').addEventListener...
